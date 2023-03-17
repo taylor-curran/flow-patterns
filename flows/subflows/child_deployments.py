c181ed69b49ac26a84065f1defd168b7732f1397
Taylor Curran <taylorcurranagtc@gmail.com>
2023-03-17 11:36:57 -0500

    Adding result persistence

--- flows/subflows/child_deployments.py
+++ flows/subflows/child_deployments.py
@@ -3,7 +3,7 @@ from prefect import flow
 # TODO: Set persistence of the child flows to True
 
 # prefect deployment build child_deployments.py:child_flow_a -n child-deployment-a -t sub-flow -a
-@flow()
+@flow(persist_result=True)
 def child_flow_a(i, sim_failure_child_flow_a):
     print(f'i: {i}')
     if sim_failure_child_flow_a:
@@ -12,7 +12,7 @@ def child_flow_a(i, sim_failure_child_flow_a):
         return {'a': 'child flow a'}
 
 # prefect deployment build child_deployments.py:child_flow_b -n child-deployment-b -t sub-flows -t child -a
-@flow()
+@flow(persist_result=True)
 def child_flow_b(i={'i': 'upstream task'}, sim_failure_child_flow_b=False):
     print(f'i: {i}')
     if sim_failure_child_flow_b:
@@ -21,11 +21,11 @@ def child_flow_b(i={'i': 'upstream task'}, sim_failure_child_flow_b=False):
         return {'b': 'child flow b'}
 
 # prefect deployment build child_deployments.py:child_flow_c -n child-deployment-c -t sub-flows -t child -a
-@flow()
+@flow(persist_result=True)
 def child_flow_c():
     return {'c': 'child flow c'}
 
 # prefect deployment build child_deployments.py:child_flow_d -n child-deployment-d -t sub-flows -t child -a
-@flow()
+@flow(persist_result=True)
 def child_flow_d():
     return {'d': 'child flow d'}
