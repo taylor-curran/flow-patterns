3f2f1dd47b72a192caf3f4c78a7d57620a026495
Taylor Curran <taylorcurranagtc@gmail.com>
2023-03-23 12:21:16 -0500

    Increasing complexity of subflows

--- flows/subflows/child_flows.py
+++ flows/subflows/child_flows.py
@@ -6,6 +6,22 @@ def task_f():
     print("task f")
     return {"f": "task f"}
 
+@task
+def task_m():
+    print("task m")
+    return {"m": "task m"}
+
+@task
+def task_n(m):
+    print(m)
+    print("task n")
+    return {"n": "task n"}
+
+@task
+def task_o():
+    print("task o")
+    return {"o": "task o"}
+
 # prefect deployment build child_flows.py:child_flow_a -n dep-child-a -t sub-flows -t child -a
 @flow(persist_result=True)
 def child_flow_a(i, sim_failure_child_flow_a):
@@ -30,6 +46,7 @@ def child_flow_b(i={"i": "upstream task"}, sim_failure_child_flow_b=False):
 # prefect deployment build child_flows.py:child_flow_d -n dep-child-d -t sub-flows -t child -a
 @flow(persist_result=True)
 def child_flow_d():
+    o = task_o()
     return {"d": "child flow d"}
 
 
@@ -38,4 +55,6 @@ def child_flow_d():
 @flow(persist_result=True)
 def child_flow_c():
     d = run_deployment("child-flow-d/dep-child-d")
+    m = task_m()
+    n = task_n(m)
     return {"c": d.state.result()}
