d855caba029e6f6e7f234661c31f1d41acca44cf
Taylor Curran <taylorcurranagtc@gmail.com>
2023-05-01 09:57:09 -0500

    adding filtering example

---
+++ getting_metadata/rest_api/filter_task_runs.py
@@ -0,0 +1,22 @@
+from prefect import get_client
+from prefect.server.schemas.core import TaskRun
+from prefect.server.schemas.filters import (
+    TaskRunFilter,
+    TaskRunFilterName,
+    DeploymentFilter,
+    DeploymentFilterName,
+)
+
+
+async def demo():
+    async with get_client() as client:
+        task_runs: list[TaskRun] = await client.read_task_runs(
+            task_run_filter=TaskRunFilter(name=TaskRunFilterName(like_="taskNamePrefix")),
+            deployment_filter=DeploymentFilter(name=DeploymentFilterName(like_="deploymentNamePrefix")),
+        )
+        
+        print(len(task_runs))
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(demo())
